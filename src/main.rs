d8ab2dc812be8b4b81be64261887118c2c5432ed
couvq <quentincouvelaire@gmail.com>
2024-12-26 10:31:53 -0500

    working with string slices example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello world");
+    let first_word = first_word(&s);
+    println!("{first_word}");
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
 }
